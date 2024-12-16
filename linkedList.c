c2034c9c3a3628707dd9b9cc88bf270d7ce67252
Mohak Gupta <mohakgupta500@gmail.com>
2024-12-17 00:24:52 +0530

    Update linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -1,136 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
- 
-struct Node
-{
-    int data;
-    struct Node *next;
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+struct Node {
+  int data;
+  struct Node *next;
 };
- 
-void linkedListTraversal(struct Node *ptr)
-{
-    while(ptr!=NULL){
-        printf("%d->", ptr->data);
-        ptr = ptr->next;
-    }
-        if(ptr==NULL){
-            printf("NULL");
-        }
-}
- 
- struct Node* insertAtBeginning(struct Node *head, int data){
-     struct Node *ptr;
-     ptr = (struct Node*)malloc(sizeof(struct Node));
-     ptr->data = data;
-     ptr->next = head;
-     return ptr;
- }
- struct Node* insertAtIndex(struct Node *head, int data, int index){
-     struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
-     struct Node *temp =head;
-     int i=0;
-     while(i<index){
-         temp = temp->next;
-         i++;
-     }
-     ptr->data = data;
-     ptr->next = temp->next;
-     temp->next = ptr;
-     return head;
- }
- struct Node* insertAtEnd(struct Node* head, int data){
-     struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
-     struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
-     temp = head;
-     while(temp->next!=NULL){
-         temp=temp->next;
-     }
-     ptr->data = data;
-     temp->next = ptr;
-     ptr->next = NULL;
-     return head;
- }
- 
- struct Node* deleteFromBeg(struct Node*head){
-     struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
-     temp = head;
-     head = head->next;
-     free(temp);
-     return head;
- }
- struct Node* deleteFromIndex(struct Node* head, int index){
-     int i=0;
-     
-     struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
-     struct Node *prev = (struct Node*)malloc(sizeof(struct Node));
-     temp = head;
-     while(i<index){
-        prev = temp;
-        temp = temp->next;
-        i++;
-     }
-     prev->next = temp->next;
-     return head;
- }
- struct Node* deleteFromEnd(struct Node* head){
-     struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
-     struct Node *prev = (struct Node*)malloc(sizeof(struct Node));
-    temp = head;
-    while(temp->next!=NULL){
-        prev = temp;
-        temp = temp->next;
-    }
-    prev->next = NULL;
-    // free(temp);
-    return head;
- }
-int main(){
-    struct Node *head;
-    struct Node *second;
-    struct Node *third;
-    struct Node *fourth;
- 
-    // Allocate memory for nodes in the linked list in Heap
-    head = (struct Node *)malloc(sizeof(struct Node));
-    second = (struct Node *)malloc(sizeof(struct Node));
-    third = (struct Node *)malloc(sizeof(struct Node));
-    fourth = (struct Node *)malloc(sizeof(struct Node));
- 
-    // Link first and second nodes
-    head->data = 7;
-    head->next = second;
- 
-    // Link second and third nodes
-    second->data = 11;
-    second->next = third;
- 
-    // Link third and fourth nodes
-    third->data = 41;
-    third->next = fourth;
- 
-    // Terminate the list at the third node
-    fourth->data = 66;
-    fourth->next = NULL;
- 
-    linkedListTraversal(head);
-    printf("\n");
-    head=insertAtBeginning(head, 12);
 
-    linkedListTraversal(head);
-    printf("\n");
-    head = insertAtIndex(head, 45, 3);
-    linkedListTraversal(head);
-    printf("\n");
-    head = insertAtEnd(head,34);
-    linkedListTraversal(head);
-    head=deleteFromBeg(head);
-    printf("\n");
-    linkedListTraversal(head);
-    head=deleteFromIndex(head, 1);
-    printf("\n");
-    linkedListTraversal(head);
-    head=deleteFromEnd(head);
-    printf("\n");
-    linkedListTraversal(head);
-    return 0;
+typedef struct Node Node;
+
+Node* createNode(int data){
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->next = NULL;
+  return newNode;
+}
+
+void traversal(Node* head){
+  Node* tmp = head;
+  while(tmp!=NULL){
+    printf("%d ", tmp->data);
+    tmp = tmp->next;
+  }
+}
+
+Node* insertatend(Node* head, int data){
+  Node* tmp = head;
+  while(tmp->next!=NULL){
+    tmp = tmp->next;
+  }
+  Node* newNode = createNode(data);
+  tmp->next = newNode;
+
+  return tmp;
+}
+
+Node* insertAtBeginning(Node* head, int data){
+  Node* newNode = createNode(data);
+  newNode->next = head;
+  return newNode;
+}
+
+Node* deleteNode(Node* head){
+  Node* tmp = head;
+  Node* prev = (Node*)malloc(sizeof(Node));
+  while (tmp->next!=NULL){
+    prev = tmp;
+    tmp = tmp->next;
+  } 
+  prev->next = NULL;
+  return head;
+}
+
+
+int main(){
+  struct Node* head = createNode(1);
+  head->next = createNode(2);
+  head->next->next = createNode(3);
+  head->next->next->next = createNode(4);
+  traversal(head);
+  insertatend(head, 5);
+  printf("\n");
+  insertatend(head, 5);
+  insertatend(head, 5);
+  head = insertAtBeginning(head, 0);
+  head = deleteNode(head);
+  traversal(head);
 }
